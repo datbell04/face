ae6f7805707505b430bcaf6338a6f576894feb65
datbell04 <letangdat17102004@gmail.com>
2025-04-04 14:34:24 +0700

    first commit

---
+++ OpenCV/build/intermediates/cxx/Debug/3o3x6f2q/logs/x86_64/build_command_opencv_jni_shared.bat
@@ -0,0 +1,5 @@
+@echo off
+"C:\\Users\\letan\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
+  -C ^
+  "D:\\workspace\\face_detected\\OpenCV\\.cxx\\Debug\\3o3x6f2q\\x86_64" ^
+  opencv_jni_shared
---
+++ OpenCV/build/intermediates/cxx/Debug/3o3x6f2q/logs/x86_64/configure_command.bat
@@ -0,0 +1,19 @@
+@echo off
+"C:\\Users\\letan\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\cmake.exe" ^
+  "-HD:\\workspace\\face_detected\\OpenCV\\libcxx_helper" ^
+  "-DCMAKE_SYSTEM_NAME=Android" ^
+  "-DCMAKE_EXPORT_COMPILE_COMMANDS=ON" ^
+  "-DCMAKE_SYSTEM_VERSION=24" ^
+  "-DANDROID_PLATFORM=android-24" ^
+  "-DANDROID_ABI=x86_64" ^
+  "-DCMAKE_ANDROID_ARCH_ABI=x86_64" ^
+  "-DANDROID_NDK=C:\\Users\\letan\\AppData\\Local\\Android\\Sdk\\ndk\\27.0.12077973" ^
+  "-DCMAKE_ANDROID_NDK=C:\\Users\\letan\\AppData\\Local\\Android\\Sdk\\ndk\\27.0.12077973" ^
+  "-DCMAKE_TOOLCHAIN_FILE=C:\\Users\\letan\\AppData\\Local\\Android\\Sdk\\ndk\\27.0.12077973\\build\\cmake\\android.toolchain.cmake" ^
+  "-DCMAKE_MAKE_PROGRAM=C:\\Users\\letan\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
+  "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=D:\\workspace\\face_detected\\OpenCV\\build\\intermediates\\cxx\\Debug\\3o3x6f2q\\obj\\x86_64" ^
+  "-DCMAKE_RUNTIME_OUTPUT_DIRECTORY=D:\\workspace\\face_detected\\OpenCV\\build\\intermediates\\cxx\\Debug\\3o3x6f2q\\obj\\x86_64" ^
+  "-DCMAKE_BUILD_TYPE=Debug" ^
+  "-BD:\\workspace\\face_detected\\OpenCV\\.cxx\\Debug\\3o3x6f2q\\x86_64" ^
+  -GNinja ^
+  "-DANDROID_STL=c++_shared"
